f5c72509bbcaa4ae9402e7afc35c193db708ce11
Артур Абдрахманов <a.arthur77777@gmail.com>
2023-01-07 22:54:54 +0300

    Добавил недостающие поля в таблицы для responses и jobs

--- src/models/jobs.py
+++ src/models/jobs.py
@@ -1,3 +1,5 @@
+import datetime
+
 from db_settings import Base
 import sqlalchemy as sa
 from sqlalchemy.orm import relationship
@@ -9,7 +11,14 @@ class Job(Base):
     id = sa.Column(sa.Integer, primary_key=True, autoincrement=True, comment="Идентификатор вакансии")
     user_id = sa.Column(sa.Integer, sa.ForeignKey('users.id'), comment="Идентификатор пользователя")
 
-    # добавьте ваши колонки сюда
+    title = sa.Column(sa.String, comment="Название вакансии")
+    description = sa.Column(sa.String, comment="Описание вакансии")
+
+    salary_from = sa.Column(sa.String, comment="Зарплата от")
+    salary_to = sa.Column(sa.String, comment="Зарплата до")
+
+    is_active = sa.Column(sa.Boolean, comment="Активна ли вакансия", default=True)
+    created_at = sa.Column(sa.DateTime, comment="Дата создания записи", default=datetime.datetime.utcnow)
 
     user = relationship("User", back_populates="jobs")
     responses = relationship("Response", back_populates="job")
