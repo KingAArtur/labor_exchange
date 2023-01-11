53c2c3cdd49ca6f1c9f7b2fab18ffcfaf010c803
Артур Абдрахманов <a.arthur77777@gmail.com>
2023-01-11 11:29:34 +0300

    Изменил тип salary_from и salary_to в схемах и БД на int, добавил валидацию
    Проверка того, что salary_from <= salary_to и salary_from >= 0

--- src/schemas/job.py
+++ src/schemas/job.py
@@ -1,6 +1,6 @@
 import datetime
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 
 class JobSchema(BaseModel):
@@ -8,8 +8,8 @@ class JobSchema(BaseModel):
     user_id: int
     title: str
     description: str
-    salary_from: str
-    salary_to: str
+    salary_from: int
+    salary_to: int
     is_active: bool = True
     created_at: datetime.datetime
 
@@ -20,5 +20,17 @@ class JobSchema(BaseModel):
 class JobInSchema(BaseModel):
     title: str
     description: str
-    salary_from: str
-    salary_to: str
+    salary_from: int
+    salary_to: int
+
+    @validator("salary_from")
+    def salary_from_is_not_negative(cls, v):
+        if v < 0:
+            raise ValueError("Зарплата не может быть меньше нуля!")
+        return v
+
+    @validator("salary_to")
+    def salary_to_is_not_less_than_from(cls, v, values, **kwargs):
+        if 'salary_from' in values and v < values["salary_from"]:
+            raise ValueError("Верхняя граница диапазона зарплаты не может быть меньше нижней границы!")
+        return v
