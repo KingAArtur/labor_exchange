cce8d2fef731b11f3778369929e5e62c0e2a6648
Артур Абдрахманов <a.arthur77777@gmail.com>
2023-01-13 13:15:47 +0300

    Создание тестовой базы данных

--- src/db_settings.py
+++ src/db_settings.py
@@ -7,8 +7,17 @@ DB_USER = os.environ.get("DB_USER", "admin")
 DB_PASS = os.environ.get("DB_PASS", "admin")
 DB_HOST = os.environ.get("DB_HOST", "localhost")
 DB_NAME = os.environ.get("DB_NAME", "labor-exchange")
+DB_STAGE = os.environ.get("DB_STAGE", "production")
+DB_PORT = os.environ.get("DB_PORT", "5432")
 
-SQLALCHEMY_DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
+if DB_STAGE == "production":
+    pass
+elif DB_STAGE == "testing":
+    DB_PORT = "5433"
+else:
+    raise ValueError(f'DB_STAGE = {DB_STAGE} is incorrect!')
+
+SQLALCHEMY_DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_async_engine(
     SQLALCHEMY_DATABASE_URL
