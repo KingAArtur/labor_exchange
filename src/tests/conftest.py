fd4e0f8ffc1389386d7acd31007ca194163f881a
Артур Абдрахманов <a.arthur77777@gmail.com>
2023-01-18 14:56:05 +0300

    Авторизация пользователя с помощью токена при тестировании

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,6 +1,8 @@
 import asyncio
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
+
+from core.security import create_access_token
 from fixtures.users import UserFactory
 from fixtures.jobs import JobFactory
 from fixtures.responses import ResponseFactory
@@ -10,6 +12,7 @@ from unittest.mock import MagicMock
 from db_settings import SQLALCHEMY_DATABASE_URL
 from httpx import AsyncClient
 from dependencies import get_db
+from schemas import TokenSchema
 
 
 @pytest_asyncio.fixture()
@@ -48,6 +51,15 @@ async def current_user(sa_session: AsyncSession):
     return new_user
 
 
+@pytest_asyncio.fixture()
+async def access_token(current_user):
+    token = TokenSchema(
+        access_token=create_access_token({"sub": current_user.email}),
+        token_type="Bearer"
+    )
+    return token
+
+
 # регистрация фабрик
 @pytest_asyncio.fixture(autouse=True)
 def setup_factories(sa_session: AsyncSession) -> None:
@@ -57,7 +69,9 @@ def setup_factories(sa_session: AsyncSession) -> None:
 
 
 @pytest_asyncio.fixture()
-async def client_app(sa_session):
+async def client_app(sa_session: AsyncSession, access_token: TokenSchema):
     app.dependency_overrides[get_db] = lambda: sa_session
+
     async with AsyncClient(app=app, base_url="http://test") as client:
+        client.headers["Authorization"] = f"Bearer {access_token.access_token}"
         yield client
